3777945ad6caa91b46f9ddd5f517564a898f64fb
delfige <delfinagerea@gmail.com>
2023-10-17 13:39:17 -0300

    commit

---
+++ carro.js
@@ -0,0 +1,34 @@
+const boton = document.getElementById("miboton");
+const textoEmergente = document.getElementById("textoEmergente");
+
+boton.addEventListener("mouseover", () => {
+    textoEmergente.style.display = "block";
+});
+
+boton.addEventListener("mouseout", () => {
+    textoEmergente.style.display = "none";
+});
+
+const boton1 = document.getElementById("miboton1");
+const textoEmergente1 = document.getElementById("textoEmergente1");
+
+boton1.addEventListener("mouseover", () => {
+    textoEmergente1.style.display = "block";
+});
+
+boton1.addEventListener("mouseout", () => {
+    textoEmergente1.style.display = "none";
+});
+
+const boton2 = document.getElementById("miboton2");
+const textoEmergente2 = document.getElementById("textoEmergente2");
+
+boton2.addEventListener("mouseover", () => {
+    textoEmergente2.style.display = "block";
+});
+
+boton2.addEventListener("mouseout", () => {
+    textoEmergente2.style.display = "none";
+});
+
+  
---
+++ components/frutas.js
@@ -0,0 +1,137 @@
+const openShopping = document.querySelector(".shopping");
+const closeShopping = document.querySelector(".closeShopping");
+const list = document.querySelector(".list");
+const listCard = document.querySelector(".listCard");
+const total = document.querySelector(".total");
+const body = document.querySelector("body");
+const quantity = document.querySelector(".quantity");
+const categoriaItems = document.querySelectorAll('.despleg');
+
+openShopping.addEventListener("click", () => {
+    body.classList.add("active");
+});
+
+closeShopping.addEventListener("click", () => {
+    body.classList.remove("active");
+});
+
+let products = [];
+let listCards = [];
+let selectedCategoria = null; 
+
+const initApp = () => {
+    // Cargar datos del carrito desde LocalStorage
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+        listCards = JSON.parse(savedCart);
+    }
+
+    const path = window.location.pathname; // Obtenemos la ruta de la URL
+    const pathSegments = path.split("/"); // Dividimos la ruta en segmentos
+    const currentPage = pathSegments[pathSegments.length - 1]; // Obtenemos la última parte de la ruta (nombre del archivo actual)
+    
+    // Mapeamos el nombre del archivo actual a la categoría correspondiente
+    const categorias = {
+        "frutas.html": "frutas",
+        "verduras.html": "verduras",
+        "ensaladas.html": "ensaladas",
+        "home.html": "home"
+    };
+
+    selectedCategoria = categorias[currentPage] || "frutas"; // Establecemos la categoría basada en la página actual
+
+    // Cargar productos desde JSON
+    fetch('../productos.json')
+        .then(response => response.json())
+        .then(data => {
+            products = data;
+            renderProductList();
+            reloadcard();
+        })
+        .catch(error => console.error('Error al cargar los productos: ', error));
+};
+
+initApp();
+
+categoriaItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+        selectedCategoria = event.currentTarget.getAttribute('data-categoria');
+        renderProductList();
+    });
+});
+
+const renderProductList = () => {
+    list.innerHTML = "";
+
+    const filteredProducts = products.filter(product => product.categoria === selectedCategoria);
+
+    filteredProducts.forEach((value, key) => {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("item");
+        newDiv.innerHTML = `
+            <img src="../assets/${value.imagen}">
+            <div class="title">${value.name}</div>
+            <div class="price">$${value.price.toFixed(2)}</div>
+            <button onclick="addToCart(${key})">Add to Cart</button>
+        `;
+
+        list.appendChild(newDiv);
+    });
+};
+const addToCart = (key) => {
+    if (listCards[key] == null) {
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+        listCards[key].price = listCards[key].quantity * products[key].price;
+        localStorage.setItem('cart', JSON.stringify(listCards));
+    } else {
+        listCards[key].quantity += 1;
+        listCards[key].price = listCards[key].quantity * products[key].price;
+        localStorage.setItem('cart', JSON.stringify(listCards));
+    }
+    reloadcard();
+};
+
+const reloadcard = () => {
+    listCard.innerHTML = "";
+    let count = 0;
+    let totalPrice = 0;
+
+    listCards.forEach((value, key) => {
+        if (value != null) {
+            totalPrice += value.price;
+            count += value.quantity;
+
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML = `
+                <div><img src="../assets/${value.imagen}"></div>
+                <div class="cardTitle">${value.name}</div>
+                <div class="cardPrice">$${value.price.toFixed(2)}</div>
+                <div>
+                    <button style="background-color: #b3db6b; padding:15px;" class="cardButton" onclick="changeQuantity(${key}, ${value.quantity - 1})"> - </button>
+                    <span style="color: black; margin:10px;">${value.quantity}</span>
+                    <button style="background-color: #b3db6b; padding:15px;" class="cardButton" onclick="changeQuantity(${key}, ${value.quantity + 1})"> + </button>
+                </div>
+            `;
+            listCard.appendChild(newDiv);
+        }
+    });
+    total.innerText = `$${totalPrice.toFixed(2)}`;
+    quantity.innerText = count;
+};
+
+
+const changeQuantity = (key, quantity) => {
+    if (quantity === 0) {
+        delete listCards[key];
+    } else {
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+        localStorage.setItem('cart', JSON.stringify(listCards));
+    }
+    reloadcard();
+};
+
+renderProductList();
+
